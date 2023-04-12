71c6b2629d57dbaadb876c00c8f3bdb20c75f4e3
Andres <andres039039@hotmail.com>
2023-04-12 12:46:46 -0400

    add head title and favicon

--- components/nav.tsx
+++ components/nav.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <Link
               href="about"
-              className="md:dark:hover:text-blue-500 block py-2 pl-3 pr-4 text-white rounded md:bg-transparent  md:p-0 dark:text-white "
+              className=" text-gray-900 md:dark:hover:text-blue-500 hover:bg-gray-100 block py-2 pl-3 pr-4  rounded md:bg-transparent  md:p-0 dark:text-white "
             >
               About
             </Link>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <link rel="icon" href="/favicon-16x16.png" type="image/png"/>
+      <title>Andr&eacute;s Beltr&aacute;n</title>
+        </Head>
       <body className='h-screen  bg-gradient-to-r from-slate-800 to-slate-400 font-mono'>
         <Main />
         <NextScript />
