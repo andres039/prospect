2e35028a5dfcc20ac7ae6df4400b1b78db8d3633
Andres <andres039039@hotmail.com>
2023-04-12 12:34:48 -0400

    make burger open and close

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState("hidden");
+  const openBurger = () => {
+    openMenu === "hidden" ? setOpenMenu("") : setOpenMenu("hidden");
+  };
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -15,6 +19,7 @@ const Navbar = () => {
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 "
           aria-controls="navbar-default"
           aria-expanded="false"
+          onClick={openBurger}
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -31,12 +36,14 @@ const Navbar = () => {
             ></path>
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={`${openMenu} text-center w-full md:block md:w-auto `}
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <Link
               href="about"
               className="md:dark:hover:text-blue-500 block py-2 pl-3 pr-4 text-white rounded md:bg-transparent  md:p-0 dark:text-white "
-
             >
               About
             </Link>
