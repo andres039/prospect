c63e352472b4da105b29c58b3c45d6afdb329e90
Andres <andres039039@hotmail.com>
2023-04-12 20:34:18 -0400

    add funny hands

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,14 @@
 export default function Home() {
   return (
 
-      <main className="flex h-96 justify-center content-center">
+      <main className="grid h-96 justify-center content-center">
+        <div className="text-6xl float-left mb-36 mt-64">👋</div>
         <h2 className=" text-7xl self-center font-semibold text-yellow-400 transfrom -rotate-2">
           Welcome
         </h2>
+        <div className="text-6xl ml-52 mt-28">👋</div>
+        <div className="text-6xl ml-32 mt-24">👋</div>
+
       </main>
 
   );
